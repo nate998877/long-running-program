24b84e7fba2e5fc70d170d7ccbd1cea2847073fa
Nate <nate998877@gmail.com>
2019-09-30 09:58:34 -0400

    added better formatting and added text to docstrings

--- dirwatcher.py
+++ dirwatcher.py
@@ -7,33 +7,29 @@ import os
 import logging
 
 
-
-
-
 filedict = {}
 exit_event = Event()
 logging.basicConfig(filename="watcher.log", level=logging.DEBUG,
-                    format='[%(levelname)8s] [%(lineno)d]: %(asctime)s - %(message)s')
-
+                    format='[%(levelname)7s] [%(lineno)d]: %(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-signames = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
-                if v.startswith('SIG') and not v.startswith('SIG_'))
 
 def init_parser():
-    """[summary]
-    
+    """Creates then returns the CLI for DirWatcher
+
     Returns:
-        [type] -- [description]
+        dict -- Parsed comand line arguments
     """
     p = argparse.ArgumentParser(
         description='Monitor a directory and log changes to monitored files')
     p.add_argument("--dir", type=str,
-                help="Absolute or relative directory to monitor. NO preceeding text on relative paths ex './'", default=os.getcwd())
+                   help="Absolute or relative directory to monitor. NO preceeding text on relative paths ex './'", default=os.getcwd())
     p.add_argument("--ext", type=str,
-                help="File extension to monitor ex .log .txt", default=".txt")
-    p.add_argument("--int", type=int, help="Timeout interval in seconds", default=1)
-    p.add_argument("magicStr", type=str, help="String to log if discovered in monitored directory")
+                   help="File extension to monitor ex .log .txt", default=".txt")
+    p.add_argument("--int", type=int,
+                   help="Timeout interval in seconds", default=1)
+    p.add_argument("magicStr", type=str,
+                   help="String to log if discovered in monitored directory")
 
     return p.parse_args()
 
@@ -49,30 +45,33 @@ def signal_handler(sig_num, frame):
     # log the associated signal name (the python3 way)
     logger.warning('Received ' + signal.Signals(sig_num).name)
     exit_event.set()
-    
 
 
 def watch_dir(args):
-    """[summary]
-    
+    """control logic for monitoring dir
+
     Arguments:
-        args {[type]} -- [description]
+        args {dict} -- args parsed from command line
     """
     file_list = []
     for file in os.listdir(os.getcwd()):
         if file.endswith(args.ext):
             file_list.append(file)
-            find_files(file)
+            update_file_dict(file)
             read_new_lines(file, args)
-    new_arr = set(file_list).difference(filedict.keys())
+    diff_set = set(filedict.keys()).difference(file_list)
+    if diff_set:
+        for item in diff_set:
+            del filedict[item]
+            logger.warn(f"{item} was deleted from {args.dir} and was removed from watch list")
 
 
 
-def find_files(file):
-    """[summary]
-    
+def update_file_dict(file):
+    """Adds File to global filedict object
+
     Arguments:
-        file {[type]} -- [description]
+        file {str} -- string representing path to file
     """
     if not file in filedict:
         logger.info(f"added file {file} to watch list")
@@ -80,29 +79,35 @@ def find_files(file):
             "mod_date": os.stat(file)[8],
             "first_read": True
         }
-    
+
 
 def read_new_lines(file, args):
-    """[summary]
-    
+    """Reads lines that have been added to any watched documents since the last
+       time this function has run. Including all lines in new files.
+
     Arguments:
-        file {[type]} -- [description]
-        args {[type]} -- [description]
+        file {str} -- string representing path to file
+        args {dict} -- parsed command line args
     """
     current_mod_date = os.stat(file)[8]
+
     if not current_mod_date == filedict[file]['mod_date'] or filedict[file]['first_read']:
         filedict[file]['first_read'] = False
-        filedict[file]['mod_date']   = current_mod_date
+        filedict[file]['mod_date'] = current_mod_date
+
         with open(file, 'r') as f:
             line_no = 0
             if 'byte_read_offset' in filedict[file].keys():
                 f.seek(filedict[file]["byte_read_offset"])
+
             for i, line in enumerate(f.readlines()):
                 line_no = i
                 if args.magicStr in line and "line_no" in filedict[file].keys():
-                    logger.debug(f"Magic string found in {file} on line {filedict[file]['line_no']+i}")
+                    logger.debug(
+                        f"Magic string found in {file} on line {filedict[file]['line_no']+i}")
                 elif args.magicStr in line:
                     logger.debug(f"Magic string found in {file} on line {i+1}")
+
             filedict[file]["byte_read_offset"] = f.tell()
             if "line_no" in filedict[file].keys():
                 filedict[file]["line_no"] += line_no
@@ -111,34 +116,28 @@ def read_new_lines(file, args):
 
 
 def main():
-    """[summary]
+    """Main function that calls other functions and contains main loop
     """
     args = init_parser()
-    os.chdir(args.dir)
+
     polling_interval = args.int
     for sig in [signal.SIGINT, signal.SIGTERM, signal.SIGQUIT]:
         signal.signal(sig, signal_handler)
 
-    logger.info(f"""
------------------------------------------------
-Dirwatcher Started @ {time.strftime("%Y/%m/%d, %H:%M:%S - %Z", time.localtime(time.time()))}
------------------------------------------------""")
-    logger.info(f"Log started with watched Dir {args.dir} Timeout {args.int} magicStr {args.magicStr} and extension {args.ext}")
-    #TODO: log adding and removal of files
+    logger.info(f"""\n{"-"*47}\nDirwatcher Started @ {time.strftime('%Y/%m/%d, %H:%M:%S - %Z', time.localtime(time.time()))}\n{"-"*47}""")
+    logger.info(f"Log started with \x7B dir : '{args.dir}', timeout : {args.int}s, magicStr : '{args.magicStr}', extension : '{args.ext}' \x7D")
 
     while not exit_event.is_set():
         try:
+            if not os.getcwd().endswith(args.dir):
+                os.chdir(args.dir)
             watch_dir(args)
         except Exception as e:
             logger.error(e)
 
         exit_event.wait(polling_interval)
 
-    logger.info(f"""
----------------------------------------------
-Dirwatcher Ended @ {time.strftime("%Y/%m/%d, %H:%M:%S - %Z", time.localtime(time.time()))}
----------------------------------------------""")
-
+    logger.info(f"\n{'-'*45}\nDirwatcher Ended @ {time.strftime('%Y/%m/%d, %H:%M:%S - %Z', time.localtime(time.time()))}\n{'-'*45}")
 
 
 if __name__ == "__main__":
